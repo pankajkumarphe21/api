20e0912d78c177686ca8537c380e120ea09aa20e
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2025-03-07 01:45:41 +0530

    initiate

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 import userModel from "../models/user.model.js";
 import jwt from 'jsonwebtoken';
 import admin from "../config/firebase.js";
+import bcrypt from 'bcryptjs';
 
 export const loginByFirebase = async (req, res) => {
     try {
@@ -10,7 +11,7 @@ export const loginByFirebase = async (req, res) => {
         const {email } = decodedToken;
         let user=await userModel.findOne({email});
         if(!user){
-            user=await userModel.create({email});
+            return res.status(400).json({message:"You are not registered with us"})
         }
         const token = jwt.sign({ uid:user._id, email }, process.env.JWT_SECRET, {
             expiresIn: "7d",
@@ -19,7 +20,7 @@ export const loginByFirebase = async (req, res) => {
         res.json({
             message: "User authenticated successfully",
             token,
-            user: { uid:user._id, email }
+            user: { userId:user._id, email }
         });
     } catch (error) {
         // console.error(error);
@@ -28,9 +29,34 @@ export const loginByFirebase = async (req, res) => {
 }
 
 export const loginUser=async(req,res)=>{
-
+    try {
+        const {email,password}=req.body;
+        const existingUser=await userModel.findOne({email}).select('+password');
+        if(!existingUser){
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+        const isCorrect =await bcrypt.compare(password,existingUser.password);
+        if(!isCorrect){
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+        return res.status(200).json({user:{email,userId:existingUser._id}});
+    } catch (error) {
+        return res.status(500).json({message:"Internal Server Error",error});
+    }
 }
 
 export const registerUser=async(req,res)=>{
-
+    try {
+        const {email,firstName,lastName,password}=req.body;
+        const existingUser=await userModel.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message:"User already registered"});
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+        const user=await userModel.create({email,firstName,lastName,password:hash});
+        return res.status(200).json({user:{email,userId:user._id}});
+    } catch (error) {
+        return res.status(500).json({message:"Internal Server Error",error});
+    }
 }
